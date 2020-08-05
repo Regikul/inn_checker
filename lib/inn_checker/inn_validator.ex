f6306d7e3dbf6b57ad461cd663f9dbc87156df5d
Maksim Krzhemenevskiy <regikul@gmail.com>
2020-08-05 19:23:38 +0700

    update function names

--- lib/inn_checker/inn_validator.ex
+++ lib/inn_checker/inn_validator.ex
@@ -15,33 +15,35 @@ defmodule InnChecker.InnValidator do
     <<digit + ?0, dec_to_asc(rest) :: binary>>
   end
 
-  def validate(inn) do
+  def valid?(inn) do
     inn = asc_to_dec(inn)
-    validate_digits_range(inn) and
-    validate_length(inn) and
+    digits_in_range?(inn) and
+    valid_length?(inn) and
       case byte_size(inn) do
-        10 -> validate_inn_10(inn)
-        12 -> validate_inn_12(inn)
+        10 -> valid_inn_10?(inn)
+        12 -> valid_inn_12?(inn)
       end
   end
 
-  def validate_digits_range(<<>>), do: true
-  def validate_digits_range(<<d :: 8, ds :: binary>>) do
-    (d >= 0) and (d <= 9) and validate_digits_range(ds)
+  def valid_digits_range?(inn), do: digits_in_range?(asc_to_dec(inn))
+
+  def digits_in_range?(<<>>), do: true
+  def digits_in_range?(<<d :: 8, ds :: binary>>) do
+    (d >= 0) and (d <= 9) and digits_in_range?(ds)
   end
 
-  def validate_length(inn) when is_list(inn), do: validate_length(List.to_string(inn))
-  def validate_length(inn) when is_binary(inn) do
+  def valid_length?(inn) when is_list(inn), do: valid_length?(List.to_string(inn))
+  def valid_length?(inn) when is_binary(inn) do
     size = byte_size(inn)
     size === @inn_long_length or size === @inn_short_length
   end
 
-  def validate_inn_10(inn) do
+  defp valid_inn_10?(inn) do
     sum = calc_sum(inn, :binary.part(@weights, 2, 10))
     sum === :binary.at(inn, 9)
   end
 
-  def validate_inn_12(inn) do
+  defp valid_inn_12?(inn) do
     sum11 = calc_sum(:binary.part(inn, 0, 11), :binary.part(@weights, 1, 11))
     sum12 = calc_sum(inn, :binary.part(@weights, 0, 12))
     sum11 === :binary.at(inn, 10) and sum12 === :binary.at(inn, 11)
--- test/inn_checker/inn_validator_test.exs
+++ test/inn_checker/inn_validator_test.exs
@@ -4,30 +4,30 @@ defmodule InnChecker.InnValidatorTest do
   alias InnChecker.InnValidator
 
   test "valid 10-digit inn" do
-    assert InnValidator.validate("7830002293")
+    assert InnValidator.valid?("7830002293")
   end
 
   test "bad 10-digit inn" do
-    assert not InnValidator.validate("7831002293")
+    assert not InnValidator.valid?("7831002293")
   end
 
   test "valid 12-digit inn" do
-    assert InnValidator.validate("500100732259")
+    assert InnValidator.valid?("500100732259")
   end
 
   test "bad 12-digit inn" do
-    assert not InnValidator.validate("500101732259")
+    assert not InnValidator.valid?("500101732259")
   end
 
   test "too short inn" do
-    assert not InnValidator.validate("123")
+    assert not InnValidator.valid?("123")
   end
 
   test "too long inn" do
-    assert not InnValidator.validate("123456789123456")
+    assert not InnValidator.valid?("123456789123456")
   end
 
   test "not an inn" do
-    assert not InnValidator.validate("50010hello1732259")
+    assert not InnValidator.valid?("50010hello1732259")
   end
 end
