9cb7302bf991a2c37a8f5a6e3159711b1866867a
Maksim Krzhemenevskiy <regikul@gmail.com>
2020-08-06 17:05:03 +0700

    aws_ec2 env

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule InnChecker.MixProject do
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
-      start_permanent: Mix.env() == :prod,
+      start_permanent: Mix.env() in [:prod, :aws_ec2],
       aliases: aliases(),
       deps: deps()
     ]
